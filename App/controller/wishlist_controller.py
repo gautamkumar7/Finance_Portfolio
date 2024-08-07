1d8e023b7399e9272cab20b52e21bc05511ee7b9
Gautam Kumar <gautamwork930@gmail.com>
2024-08-08 00:03:46 +0530

    added swagger for wishlist

--- App/controller/wishlist_controller.py
+++ App/controller/wishlist_controller.py
@@ -1,5 +1,5 @@
 # controller/wishlist_controller.py
-
+from flasgger import swag_from
 from flask import Blueprint, jsonify, request
 
 from services.wishlist_service import WishlistService  # Import the WishlistService
@@ -8,6 +8,39 @@ wishlist_bp = Blueprint('wishlist', __name__)
 
 
 @wishlist_bp.route('/wishlist', methods=['GET'])
+@swag_from({
+    'parameters': [
+        {
+            'name': 'number',
+            'in': 'query',
+            'type': 'integer',
+            'required': True,
+            'description': 'The number representing the type of wishlist (1 or 2)'
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'A list of wishlist items',
+            'schema': {
+                'type': 'array',
+                'items': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'integer'},
+                        'stock_name': {'type': 'string'},
+                        'current_price': {'type': 'number'}
+                    }
+                }
+            }
+        },
+        400: {
+            'description': 'Invalid number parameter'
+        },
+        404: {
+            'description': 'No wishlist data found'
+        }
+    }
+})
 def get_wishlist():
     number = request.args.get('number', type=int)
     if number not in [1, 2]:
