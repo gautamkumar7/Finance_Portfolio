89c763c05853df7c518cd542db09cf30b868d13f
Gautam Kumar <gautamwork930@gmail.com>
2024-08-10 17:33:11 +0530

    added drop down in navbar

--- frontend_react/src/components/Nav.tsx
+++ frontend_react/src/components/Nav.tsx
@@ -5,25 +5,44 @@ import hero from "/public/hero.jpeg";
 import landing from "/public/landing.jpeg";
 import Link from "next/link";
 import ThemeToggleButton from "./ThemeToggleButton";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const Nav = () => {
   return (
     <div className="relative">
       <div className="bg-gradient-to-r from-black to bg-gray-500 w-full h-32 flex items-center justify-around p-4">
-        <Link href='/' className="text-white font-bold text-4xl">PortfolioX</Link>
+        <Link href="/" className="text-white font-bold text-4xl">
+          PortfolioX
+        </Link>
         <div className="flex space-x-3 items-center">
           <BellDot color="white" size={24} />
           <div className="relative w-10 h-10">
-            <Image
-              className="rounded-full"
-              src={hero}
-              layout="fill"
-              objectFit="cover"
-              alt="Gautam"
-            />
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Image
+                  className="rounded-full cursor-pointer"
+                  src={hero}
+                  layout="fill"
+                  objectFit="cover"
+                  alt="Gautam"
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mt-4 mr-10">
+                <DropdownMenuItem>
+                  <Link href="/team">My Teamates</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>Setting</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
           <ThemeToggleButton />
-          {/* <span className="text-white text-lg">User</span> */}
         </div>
       </div>
       <div className="absolute left-1/2 transform -translate-x-1/2 border-gray-800 rounded-lg border-2  w-3/5 -bottom-6">
@@ -41,7 +60,7 @@ const Nav = () => {
             <Link className="btn" href="/Analytics">
               Analytics
             </Link>
-            <Link className="btn" href="/Trade">
+            <Link className="btn" href="/Transactions">
               Trades
             </Link>
           </ul>
